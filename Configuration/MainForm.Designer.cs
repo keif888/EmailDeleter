02d603986cc9cf84870ef03f8627d11cf5c71109
Keith Martin <keith.martin@outlook.com.au>
2025-01-15 19:09:12 +1000

    Add Configurator

---
+++ Configuration/MainForm.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Configuration
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbKey = new System.Windows.Forms.TextBox();
+            this.btnKeyGen = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbKeyBase64 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbValue = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbKey
+            // 
+            this.tbKey.Location = new System.Drawing.Point(138, 6);
+            this.tbKey.Name = "tbKey";
+            this.tbKey.Size = new System.Drawing.Size(200, 20);
+            this.tbKey.TabIndex = 1;
+            this.tbKey.Leave += new System.EventHandler(this.tbKey_Leave);
+            // 
+            // btnKeyGen
+            // 
+            this.btnKeyGen.Location = new System.Drawing.Point(344, 4);
+            this.btnKeyGen.Name = "btnKeyGen";
+            this.btnKeyGen.Size = new System.Drawing.Size(75, 23);
+            this.btnKeyGen.TabIndex = 2;
+            this.btnKeyGen.Text = "Gen Key";
+            this.btnKeyGen.UseVisualStyleBackColor = true;
+            this.btnKeyGen.Click += new System.EventHandler(this.btnKeyGen_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Key";
+            // 
+            // tbKeyBase64
+            // 
+            this.tbKeyBase64.Location = new System.Drawing.Point(138, 33);
+            this.tbKeyBase64.Name = "tbKeyBase64";
+            this.tbKeyBase64.Size = new System.Drawing.Size(498, 20);
+            this.tbKeyBase64.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Key Base64";
+            // 
+            // tbValue
+            // 
+            this.tbValue.Location = new System.Drawing.Point(138, 59);
+            this.tbValue.Name = "tbValue";
+            this.tbValue.Size = new System.Drawing.Size(200, 20);
+            this.tbValue.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Value";
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(344, 57);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 4;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Result";
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(138, 85);
+            this.tbResult.Name = "tbResult";
+            this.tbResult.ReadOnly = true;
+            this.tbResult.Size = new System.Drawing.Size(498, 20);
+            this.tbResult.TabIndex = 7;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(425, 57);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 5;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 115);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbValue);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbKeyBase64);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnKeyGen);
+            this.Controls.Add(this.tbKey);
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "Config";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbKey;
+        private System.Windows.Forms.Button btnKeyGen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbKeyBase64;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbValue;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.Button btnDecrypt;
+    }
+}
+
---
+++ Configuration/MainForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Configuration
+{
+    public partial class FormMain : Form
+    {
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKeyGen_Click(object sender, EventArgs e)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.GenerateKey();
+                tbKeyBase64.Text = Convert.ToBase64String(aes.Key);
+                tbKey.Text = System.Text.Encoding.ASCII.GetString(aes.Key);
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            byte[] value = System.Text.Encoding.UTF8.GetBytes(tbValue.Text);
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Convert.FromBase64String(tbKeyBase64.Text);
+                aes.GenerateIV();
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    ms.Write(aes.IV, 0, aes.IV.Length);
+                    using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(value, 0, value.Length);
+                    }
+                    tbResult.Text = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+        }
+
+        private void tbKey_Leave(object sender, EventArgs e)
+        {
+            tbKeyBase64.Text = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(tbKey.Text));
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            byte[] value = Convert.FromBase64String(tbValue.Text);
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Convert.FromBase64String(tbKeyBase64.Text);
+                byte[] iv = new byte[aes.IV.Length];
+                int numBytesToRead = aes.IV.Length, encryptLength = value.Length - aes.IV.Length;
+                Array.Copy(value, 0, iv, 0, numBytesToRead);
+                aes.IV = iv;
+                byte[] encryptedBytes = new byte[encryptLength];
+                Array.Copy(value, numBytesToRead, encryptedBytes, 0, encryptLength);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(encryptedBytes, 0, encryptedBytes.Length);
+                    }
+                    tbResult.Text = System.Text.Encoding.UTF8.GetString(ms.ToArray());
+                }
+            }
+        }
+    }
+}
